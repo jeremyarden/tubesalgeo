0f8af1acdf41e3f3ba5da2e19c80fb761c462d2a
Jeremy Arden <43499071+jeremyarden@users.noreply.github.com>
2018-09-28 01:04:59 +0700

    Update GaussianEl.java

--- GaussianEl.java
+++ GaussianEl.java
@@ -1,67 +1,70 @@
 import java.util.Scanner;
 
-public static void GaussianEl(double[][] M, double[] N)
+public class GaussianEl()
 {
-  int i, j, k, l, m, n, o, p, r, c;
-  int max, dvdr;
-
-  //Pivot row
-  row = M.length;
-  col = M[0].length;
-
-  if (row > col)
+  public static void GaussianEl(double[][] M, double[] N)
   {
-    i = row;
-  }
-  else
-  {
-    i = col;
-  }
-  //Pivot row
-  for (j = 0; j < i; j++)
-  {
-    max = j;
-    for (k = j + 1; k < i; k++)
+    int i, j, k, l, m, n, o, p, r, c;
+    int max, dvdr;
+  
+    //Pivot row
+    row = M.length;
+    col = M[0].length;
+  
+    if (row > col)
     {
-      if (Math.abs(M[k][j]) > Math.abs(M[max][j]))
-      {
-        max = k;
-      }
+      i = row;
     }
-
-    double[] temp1;
-    temp1 = M[k];
-    M[k] = M[max];
-    M[max] = temp1;
-
-    double temp2;
-    temp2 = N[k];
-    N[k] = N[max];
-    N[max] = temp2;
-    //Biar elemen pivotny jadi 1
-    l = j;
-    while ((l < col) && (M[j][l] == 0))
+    else
     {
-      l++;
+      i = col;
     }
-    if (l < col)
+    //Pivot row
+    for (j = 0; j < i; j++)
     {
-      dvdr = M[j][l];
-      for (m = j; m < col; m++)
+      max = j;
+      for (k = j + 1; k < i; k++)
       {
-        M[j][m] = M[j][m] / dvdr;
+        if (Math.abs(M[k][j]) > Math.abs(M[max][j]))
+        {
+          max = k;
+        }
       }
-      N[j] = N[j] / dvdr;
-    }
-    //Biar bawahny jadi 0
-    for (n = j + 1; n < row; n++)
-    {
-      dvdr = M[n][l] / M[j][l];
-      for (o = 0; o < col; o++)
+  
+      double[] temp1;
+      temp1 = M[k];
+      M[k] = M[max];
+      M[max] = temp1;
+  
+      double temp2;
+      temp2 = N[k];
+      N[k] = N[max];
+      N[max] = temp2;
+      //Biar elemen pivotny jadi 1
+      l = j;
+      while ((l < col) && (M[j][l] == 0))
       {
-        M[n][o] = M[n][o] - (dvdr * M[j][o]);
+        l++;
       }
-      N[n] = N[n] - (dvdr * N[j]);
+      if (l < col)
+      {
+        dvdr = M[j][l];
+        for (m = j; m < col; m++)
+        {
+          M[j][m] = M[j][m] / dvdr;
+        }
+        N[j] = N[j] / dvdr;
+      }
+      //Biar bawahny jadi 0
+      for (n = j + 1; n < row; n++)
+      {
+        dvdr = M[n][l] / M[j][l];
+        for (o = 0; o < col; o++)
+        {
+          M[n][o] = M[n][o] - (dvdr * M[j][o]);
+        }
+        N[n] = N[n] - (dvdr * N[j]);
+      }
     }
   }
 }
